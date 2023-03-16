07981e966cc31812a1b9b83a221f61e08c432ffd
Fahir Muminovic <muminovicfahir998@gmail.com>
2023-03-16 15:44:43 +0100

    .

--- tranzicija.cpp
+++ tranzicija.cpp
@@ -17,20 +17,50 @@ Tranzicija::Tranzicija(int koordinataX, int koordinataY,int duzina, int rotacija
     setFlag(QGraphicsItem::ItemIsMovable);
 }
 
-void Tranzicija::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget){
-    QPen linije;
-    QBrush boja;
-    QPainterPath path = shape();
-    QPolygonF strelica(shape());
+void Tranzicija::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
 
+    QPainterPath strelica(shape());
 
+    // Save painter state
+    painter->save();
+
+    // Translate painter to the top-left corner of the scene
+    painter->translate(0, 0);
+
+    // Translate painter to the center of the Tranzicija item
+    QPointF center = strelica.boundingRect().center();
+    painter->translate(center);
+
+    // Rotate painter by the rotation of the Tranzicija item
+    painter->rotate(rotacija);
+
+    // Translate painter back to the top-left corner of the Tranzicija item
+    painter->translate(- center);
+
+    // Set the color and pen for drawing the Tranzicija item
+    QPen pen(Qt::black);
+    painter->setPen(pen);
+    QBrush brush(Qt::white);
+    painter->setBrush(brush);
+
+    // Draw the Tranzicija item as a polygon
+    painter->drawPath(shape());
+
+    // Restore painter state
+    painter->restore();
 }
+
 
 // funkcija koja definira granice elementa
 QRectF Tranzicija::boundingRect() const
 {
-
+    qreal penWidth = 1;
+    return QRectF(koordinataX - penWidth/2, koordinataY - penWidth/2, duzina + penWidth, duzina + penWidth);
 }
+
 
 
 QPainterPath Tranzicija::shape() const
